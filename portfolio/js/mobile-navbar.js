fef0e332ffa27c5ec5d4c223c6c01496256f012a
Daniel Luiz <daniel_cbamt@hotmail.com>
2022-01-20 18:31:42 -0400

    alguns ajustes e proxima atualização: filtro para background e ajustes na página para celular "

--- portfolio/js/mobile-navbar.js
+++ portfolio/js/mobile-navbar.js
@@ -2,22 +2,24 @@ class MobileNavbar {
   constructor(mobileMenu, navList, navLinks) {
     this.mobileMenu = document.querySelector(mobileMenu);
     this.navList = document.querySelector(navList);
-    this.navLinks = document.querySelector(navLinks);
+    this.navLinks = document.querySelectorAll(navLinks);
     this.activeClass = "active";
     this.handleClick = this.handleClick.bind(this);
   }
 
   animateLinks() {
     this.navLinks.forEach((link, index) => {
-      console.log(index);
       link.style.animation
         ? (link.style.animation = "")
-        : (link.style.animation = `navLinkFase 0.5s ease forwards 0.3s`);
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.3
+          }s`);
     });
   }
 
   handleClick() {
     this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
     this.animateLinks();
   }
 
@@ -36,6 +38,6 @@ class MobileNavbar {
 const mobileNavbar = new MobileNavbar(
   ".mobile-menu",
   ".nav-list",
-  "nav-list li"
+  ".nav-list li"
 );
 mobileNavbar.init();
